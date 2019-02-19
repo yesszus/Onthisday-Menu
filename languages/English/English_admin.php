6228b6c614377b35d41451b64ca8e251272d9730
Gábor <yesszus@gmail.com>
2019-02-19 16:09:59 +0100

    Add files via upload

--- languages/English/English_admin.php
+++ languages/English/English_admin.php
@@ -1,1 +1,168 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system English Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2019/02/19 14:49:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus
++-----------------------------------------------------------------------+
+*/
+define(OTDLAN_CAP, "On This Day ...");
+define(OTD_MORE,"[more]");
+define(OTD_MONTHLIST,"January,February,March,April,May,June,July,August,September,October,November,December");
 
+// This is the string displayed if "showifempty" is set:
+define(OTDLAN_DEFAULT, "Nothing much happened.");
+
+define(OTD_01, "You are not permitted to view this information");
+define(OTD_02, "Every year");
+define(OTD_03, "In");
+define(OTD_04, "On This Day");
+
+define(OTD_MONTHS,"January,February,March,April,May,June,July,August,September,October,November,December");
+
+define(OTD_H01,"On This Day Menu");
+define(OTD_H02,"Preferences");
+define(OTD_H03,"User Class");
+define(OTD_H04,"Which user class will see the menu and full details");
+define(OTD_H05,"Show menu on empty days");
+define(OTD_H06,"Display the menu even when there are no events on that day.");
+define(OTD_H07,"Characters in Menu");
+define(OTD_H08,"The maximum number of characters to display in the menu (excess is shown as [more])");
+define(OTD_H09,"Date Format");
+define(OTD_H10,"The format of the date in the menu");
+
+define(OTD_H11,"Event Entries");
+define(OTD_H12,"Calendar Control");
+define(OTD_H13,"Use the calendar to move to the day you wish to edit. Click on Add for a new event. Click the corresponding Edit or Delete icon for that action");
+define(OTD_H14,"Title");
+define(OTD_H15,"A brief description of this event (max 200 chars)");
+define(OTD_H16,"Event Date");
+define(OTD_H17,"Enter the day and month plus, optionally, the year of the event.");
+define(OTD_H18,"Full Text");
+define(OTD_H19,"An extended description of the event");
+
+define(OTD_H21,"Import Export Events");
+define(OTD_H22,"Available files");
+define(OTD_H23,"Select the file to import. CVS must be in the format specified.ICal files must be specified with .ics extention");
+define(OTD_H24,"Export to CVS");
+define(OTD_H25,"Name of the file to export to (include the .cvs in the file name)");
+define(OTD_H26,"Event Date");
+define(OTD_H27,"Enter the day and month plus, optionally, the year of the event.");
+define(OTD_H28,"Full Text");
+define(OTD_H29,"An extended description of the event");
+define(OTD_H30,"Check for updates");
+define(OTD_H31,"Check for the author's web site for updates");
+
+define(OTD_A01,"On This Day Menu");
+define(OTD_A02,"Preferences");
+define(OTD_A03,"Entries");
+
+define(OTD_A04,"Settings saved");
+define(OTD_A05,"On This Day Menu Preferences");
+define(OTD_A06,"Show menu on empty days");
+define(OTD_A07,"Yes");
+define(OTD_A08,"No");
+define(OTD_A09,"Save Changes");
+define(OTD_A10,"User Class");
+define(OTD_A11,"Editing events on");
+
+define(OTD_A12,"Title");
+define(OTD_A13,"Day");
+define(OTD_A14,"Month");
+define(OTD_A15,"Year");
+define(OTD_A16,"Full Text");
+define(OTD_A17,"Event Date");
+define(OTD_A18,"Maximum characters in menu");
+define(OTD_A19,"Date format");
+define(OTD_A20,"Import/Export");
+define(OTD_A21,"Add New");
+define(OTD_A22,"Edit this record");
+define(OTD_A23,"Delete this record");
+define(OTD_A24,"This day's Events");
+define(OTD_A25,"No events defined");
+define(OTD_A26,"You are about to delete the following record");
+define(OTD_A27,"Click OK to delete or cancel to exit.");
+define(OTD_A28,"OK");
+define(OTD_A29,"Cancel");
+define(OTD_A30,"Delete Entry");
+
+define(OTD_A31,"Day");
+define(OTD_A32,"Month");
+define(OTD_A33,"Year");
+
+define(OTD_A40,"Import Export Events");
+define(OTD_A41,"Available CSV and ICS files");
+define(OTD_A42,"Import");
+define(OTD_A43,"Export");
+define(OTD_A44,"CSV Data needs to be in the form <br />&nbsp;&nbsp;&nbsp;Brief Text (unformatted max length 200 characters)<br />&nbsp;&nbsp;&nbsp;Date (YYYY-MM-DD)<br />&nbsp;&nbsp;&nbsp;Full text. (With XHTML formatting max length 4096 characters)<br />The separator is , and each field should be surrounded by a double quote (\") ");
+define(OTD_A45,"Import files");
+define(OTD_A46,"Export File to CVS");
+define(OTD_A47,"Export file name");
+define(OTD_A48,"Select File");
+define(OTD_A49,"Imported");
+define(OTD_A50,"You must specify a file to import");
+define(OTD_A51,"Unable to open the file");
+define(OTD_A52,"You must specify a file to export to");
+define(OTD_A53,"Unable to write the file. Check the csv folder is writable (chmod 777 for example).");
+define(OTD_A54,"Read Me");
+define(OTD_A55,"Check for Updates");
+define(OTD_A56,"adding events on.");
+define(OTD_A57,"Plugin informations.");
+
+define(OTD_P01,"Configure On This Day Menu");
+define(OTD_P02,"Displays events that happened today.");
+define(OTD_P03,"Installation Successful..");
+define(OTD_P04, "On This Day");
+
+define("EVERSION_U1", "plugin.php does not exist in the plugin directory");
+define("EVERSION_U2", "e_update.php does not exist in the plugin directory");
+define("EVERSION_U3", "Checking for updates to plugin :");
+define("EVERSION_U4", "The Current Version on your server is :");
+define("EVERSION_U5", "Check for Updates");
+define("EVERSION_U6", "Unable to reach update site to check for version information");
+define("EVERSION_U7", "There is a new version available");
+define("EVERSION_U8", "However, it is a beta version.");
+define("EVERSION_U9", "You are up to date. You have the current version installed.");
+define("EVERSION_U10", "The update site has no details on this plugin. Contact the plugin author directly.");
+define("EVERSION_U11", "Checking for Updates");
+define("EVERSION_U12", "Results");
+define("EVERSION_U13", "View the details on this version or download it at");
+define("EVERSION_U14", "Click Here");
+define("EVERSION_U15", "Description :");
+define("EVERSION_U16", "Update Check");
+define("EVERSION_U17", "It was released on :");
+define("EVERSION_U18", "Plugin Author :");
+define("EVERSION_U19", "Plugin Manager reports that version ");
+define("EVERSION_U20", "is installed. Check the Plugin Manager.");
+define("EVERSION_U21", "Stable");
+
+define(OTD_R01,"");
+define(OTD_R02,"Plugin");
+define(OTD_R03,"");
+define(OTD_R04,"Author");
+define(OTD_R05,"");
+define(OTD_R06,"Version");
+define(OTD_R07,"");
+define(OTD_R08,"Status");
+define(OTD_R09,"Release");
+define(OTD_R10,"Disclaimer");
+define(OTD_R11,"No responsibility can be accepted for the failure of this plugin in any way shape or form. You use entirely at your own risk.");
+define(OTD_R12,"Copyright");
+define(OTD_R13,"Barry Keal 2007.");
+define(OTD_R14,"License");
+define(OTD_R15,"GPL");
+define(OTD_R16,"Website");
+define(OTD_R17,"www.keal.me.uk");
+define(OTD_R18,"Support");
+define(OTD_R19,"Support for this plug-in is <strong>ONLY</strong> available at <a href='http://www.keal.me.uk'>My Website</a> and nowhere else.");
+define(OTD_R20,"Documentation");
+define(OTD_R21,"All documentation is on the <a href='http://wiki.e107.org/?title=On_This_Day'>e107 wiki</a> Feel free to contribute to the documentation on there.");
+define(OTD_R22,"Just for Info");
+define(OTD_R23,"For editing I use PHP Edit from <a href='http://www.waterproof.fr/'>Waterproof Software</a>");
+define(OTD_R24,"If you find this plugin useful then please consider making a <b>donation</b> at <a href='http://www.keal.me.uk'>My Website</a>  ");
+define(OTD_R25,"Donations");
+?>
---
+++ languages/English/English_global.php
@@ -0,0 +1,18 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system English Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2019/02/19 14:49:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus
++-----------------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_ONTHISDAY_NAME", "On This Day");
+define("LAN_PLUGIN_ONTHISDAY_SUMMARY", "Displays events that happened today.");
+define("LAN_PLUGIN_ONTHISDAY_DESC", "Displays events that happened today.");
+
+?>
