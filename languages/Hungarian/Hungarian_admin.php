ea67fbabe7596f8b80ba71c50027acf1ded49a30
Gábor <yesszus@gmail.com>
2019-02-19 16:11:32 +0100

    Add files via upload

---
+++ languages/Hungarian/Hungarian_admin.php
@@ -0,0 +1,174 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system English Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2019/02/19 14:49:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus
++-----------------------------------------------------------------------+
+*/
+define(OTDLAN_CAP, "Ezen a napon történt...");
+define(OTD_MORE,""); // [több]
+define(OTD_MONTHLIST,"Január,Február,Március,Április,Május,Junius,Julius,Augusztus,Szeptember,Október,November,December");
+
+// This is the string displayed if "showifempty" is set:
+define(OTDLAN_DEFAULT, "Erre a napra nincs megjeleníthető bejegyzés.");
+
+define(OTD_01, "You are not permitted to view this information");
+define(OTD_02, "Every year");
+define(OTD_03, "");
+define(OTD_04, "Ezen a napon");
+
+
+define(OTD_MONTHS,"Január,Február,Március,Április,Május,Junius,Julius,Augusztus,Szeptember,Október,November,December");
+
+define(OTD_H01,"Ezen A Napon Menü");
+define(OTD_H02,"Beállítások");
+define(OTD_H03,"Láthatják");
+define(OTD_H04,"Azon felhasználók megadása, akik láthatják a bejegyzéseket.");
+define(OTD_H05,"A menü megjelenítése, ha az adott napra nincs megjeleníthető vonatkozó esemény");
+define(OTD_H06,"A menü megjelenítése akkor is, ha az adott napon nincs esemény.");
+define(OTD_H07,"Karakterek száma a menüben");
+define(OTD_H08,"The maximum number of characters to display in the menu (excess is shown as [more])");
+define(OTD_H09,"Dátum Formátum");
+define(OTD_H10,"A menüben megjelenő dátumformátum.");
+
+define(OTD_H11,"Esemény bejegyzések");
+define(OTD_H12,"Calendar Control");
+define(OTD_H13,"Use the calendar to move to the day you wish to edit. Click on Add for a new event. Click the corresponding Edit or Delete icon for that action");
+define(OTD_H14,"Cím");
+define(OTD_H15,"A brief description of this event (max 200 chars)");
+define(OTD_H16,"Esemény dátuma");
+define(OTD_H17,"Enter the day and month plus, optionally, the year of the event.");
+define(OTD_H18,"Teljes szöveg");
+define(OTD_H19,"An extended description of the event");
+
+
+define(OTD_H21,"Események importálása/exportálása");
+define(OTD_H22,"Elérhető fájlok");
+define(OTD_H23,"Válassza ki az importálandó fájlt. CVS must be in the format specified.ICal files must be specified with .ics extention");
+define(OTD_H24,"Export to CVS");
+define(OTD_H25,"Name of the file to export to (include the .cvs in the file name)");
+define(OTD_H26,"Esemény dátuma");
+define(OTD_H27,"Enter the day and month plus, optionally, the year of the event.");
+define(OTD_H28,"Teljes szöveg");
+define(OTD_H29,"An extended description of the event");
+define(OTD_H30,"Frissítések keresése");
+define(OTD_H31,"A plugin új verziójának keresése, ha elérhető.");
+
+define(OTD_A01,"Ezen A Napon Menü");
+define(OTD_A02,"Beállítások");
+define(OTD_A03,"Bejegyzések");
+
+define(OTD_A04,"A beállítások elmentve!");
+define(OTD_A05,"Ezen A Napon Menü Beállítások");
+define(OTD_A06,"A menü megjelenítése, ha az adott napra nincs megjeleníthető vonatkozó esemény");
+define(OTD_A07,"Igen");
+define(OTD_A08,"Nem");
+define(OTD_A09,"Mentés");
+define(OTD_A10,"Láthatják");
+define(OTD_A11,"napra vonatkozó esemény szerkesztése.");
+
+define(OTD_A12,"Cím");
+define(OTD_A13,"Nap");
+define(OTD_A14,"Hónap");
+define(OTD_A15,"Év");
+define(OTD_A16,"Teljes Szöveg");
+define(OTD_A17,"Esemény Dátuma");
+define(OTD_A18,"Maximum karakterek száma a menüben");
+define(OTD_A19,"Dátum formátum");
+define(OTD_A20,"Importálás/Exportálás");
+define(OTD_A21,"Új hozzáadása");
+define(OTD_A22,"Bejegyzés szerkesztése");
+define(OTD_A23,"Bejegyzés törlése");
+define(OTD_A24,"A mai napra vonatkozó események");
+define(OTD_A25,"A mai napra még nincs esemény bejegyzés.");
+define(OTD_A26,"Ön a következő bejegyzés törlésére készül:");
+define(OTD_A27,"Klikk az OK gombra a törléshez vagy kattintson a Mégsem gombra a törlés visszavonásához.");
+define(OTD_A28,"OK");
+define(OTD_A29,"Mégsem");
+define(OTD_A30,"Bejegyzés törlése");
+
+define(OTD_A31,"Nap");
+define(OTD_A32,"Hó");
+define(OTD_A33,"Év");
+
+
+define(OTD_A40,"Események Importálása/Exportálása");
+define(OTD_A41,"Elérhető CSV és ICS fájlok:");
+define(OTD_A42,"Importálás");
+define(OTD_A43,"Exportálás");
+define(OTD_A44,"CSV Data needs to be in the form <br />&nbsp;&nbsp;&nbsp;Brief Text (unformatted max length 200 characters)<br />&nbsp;&nbsp;&nbsp;Date (YYYY-MM-DD)<br />&nbsp;&nbsp;&nbsp;Full text. (With XHTML formatting max length 4096 characters)<br />The separator is , and each field should be surrounded by a double quote (\") ");
+define(OTD_A45,"Fájlok importálása");
+define(OTD_A46,"Fájl exportálása CVS fájlba");
+define(OTD_A47,"Az exportálandó fájl neve:");
+define(OTD_A48,"Fájl kiválasztása");
+define(OTD_A49,"A fájl sikeresen importálva lett!");
+define(OTD_A50,"You must specify a file to import");
+define(OTD_A51,"Unable to open the file");
+define(OTD_A52,"You must specify a file to export to");
+define(OTD_A53,"Unable to write the file. Check the csv folder is writable (chmod 777 for example).");
+define(OTD_A54,"Olvass el");
+define(OTD_A55,"Frissítések keresése");
+define(OTD_A56,"napra vonatkozó esemény hozzáadása.");
+define(OTD_A57,"Plugin információk.");
+
+define(OTD_P01,"Ezen A Napon Menü Beállítás");
+define(OTD_P02,"Az aktuális napra vonatkozó eseményeket jeleníti meg.");
+define(OTD_P03,"Sikeres telepítés..");
+define(OTD_P04, "Ezen A Napon");
+
+define("EVERSION_U1", "A plugin.php nem található a plugin mappában");
+define("EVERSION_U2", "Az e_update.php nem található a plugin mappában");
+define("EVERSION_U3", "Frissítések ellenőrzése a kiegészítőhöz :");
+define("EVERSION_U4", "A jelenlegi verzió a szervereden :");
+define("EVERSION_U5", "Frissítések ellenőrzése");
+define("EVERSION_U6", "Nem érhető el a frissítési szerver a frissítések ellenőrizéséhez.");
+define("EVERSION_U7", "Elérhető az új verzió");
+define("EVERSION_U8", "Nos, ez béta verzió. Csak akkor használd, ha tudod mit csinálsz!");
+define("EVERSION_U9", "A kiegészítő naprakész. A legfrissebb verzióval rendelkezel.");
+define("EVERSION_U10", "A frissítési szerveren nincsenek adatok a kiegészítőről.<br/>Vedd fel a kapcsolatot a készítővel.");
+define("EVERSION_U11", "Frissítések ellenőrzése");
+define("EVERSION_U12", "Végeredmény");
+define("EVERSION_U13", "Nézd meg a részletes ismertetést, vagy töltsd le most: ");
+define("EVERSION_U14", "Klikkelj ide");
+define("EVERSION_U15", "Ismertetés :");
+define("EVERSION_U16", "Frissítés ellenőrzés");
+define("EVERSION_U17", "Kiadva :");
+define("EVERSION_U18", "Kiegészítő készítője");
+define("EVERSION_U19", "A Plugin Manager jelentése a verzióról ");
+define("EVERSION_U20", "telepítve. Nézd meg a Plugin Managert.");
+define("EVERSION_U21", "Stabil");
+define("EVERSION_U22", "A telepített verzió régebbi, mint jelenlegi friss verzió.");
+define("EVERSION_U23","Elérhető verzió:  ");
+
+
+define(OTD_R01,"Ezen a napon");
+define(OTD_R02,"Plugin");
+define(OTD_R03,"Ezen a napon");
+define(OTD_R04,"Szerző");
+define(OTD_R05,"Father Barry");
+define(OTD_R06,"Verzió");
+define(OTD_R07,"1.4");
+define(OTD_R08,"Állapot");
+define(OTD_R09,"Kiadva");
+define(OTD_R10,"Figyelmeztetés");
+define(OTD_R11,"Semmilyen felelősséget nem vállalok a plugin használatából eredendő károkért!<br>Mindenki csak saját felelősségére használja!");
+define(OTD_R12,"Copyright");
+define(OTD_R13,"Barry Keal 2007.");
+define(OTD_R14,"Licensz");
+define(OTD_R15,"GPL");
+define(OTD_R16,"Weboldal");
+define(OTD_R17,"www.keal.me.uk");
+define(OTD_R18,"Támogatás");
+define(OTD_R19,"A plugin frissítések és segítség nyújtás, kizárólag <strong>CSAK</strong> a <a target='_blank' href='http://www.keal.me.uk'>Weboldalamon</a> érhető el.");
+define(OTD_R20,"Dokumentáció");
+define(OTD_R21,"All documentation is on the <a href='http://wiki.e107.org/?title=On_This_Day'>e107 wiki</a> Feel free to contribute to the documentation on there.");
+define(OTD_R22,"Just for Info");
+define(OTD_R23,"For editing I use PHP Edit from <a href='http://www.waterproof.fr/'>Waterproof Software</a>");
+define(OTD_R24,"Ha tetszik a munkám, adományozással <b>támogathatsz</b> a <a target='_blank' href='http://www.keal.me.uk'>Weboldalamon</a>.");
+define(OTD_R25,"Adomány");
+?>
--- languages/Hungarian/Hungarian_global.php
+++ languages/Hungarian/Hungarian_global.php
@@ -1,1 +1,18 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system English Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2019/02/19 14:49:43
+|
+|        $Author: Yesszus $
+|        Web: https://github.com/yesszus
++-----------------------------------------------------------------------+
+*/
 
+define("LAN_PLUGIN_ONTHISDAY_NAME", "Ezen A Napon");
+define("LAN_PLUGIN_ONTHISDAY_SUMMARY", "Az aktuális napra vonatkozó eseményeket jeleníti meg.");
+define("LAN_PLUGIN_ONTHISDAY_DESC", "Az aktuális napra vonatkozó eseményeket jeleníti meg.");
+
+?>
