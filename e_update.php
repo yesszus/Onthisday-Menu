09f98b61ce43a5a0d39f7e9444c8562065a6fb4a
Gábor <yesszus@gmail.com>
2019-02-20 10:39:08 +0100

    Update e_update.php

--- e_update.php
+++ e_update.php
@@ -1,4 +1,4 @@
 <?php
 // Set this URL to the location of your eversion xml file
-$evrsn_url="http://yesszus.tk/xml/onthisday_menu.xml";
+$evrsn_url="http://yesszus.bplaced.net/xml/onthisday_menu.xml";
 ?>
